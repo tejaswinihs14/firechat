d1fffaa4bdf05b2fd7e6bf6951404bfb70ecb260
DESKTOP-5RTJJSJ\tejug <tejugowda14@gmail.com>
2021-05-14 12:31:10 +0530

    firechat

---
+++ firechat-vue/src/db.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCE_56r48rkS_1SFSgqYiZLU_AcMmNtfHg",
+  authDomain: "firevuechat-dc68b.firebaseapp.com",
+  projectId: "firevuechat-dc68b",
+  storageBucket: "firevuechat-dc68b.appspot.com",
+  messagingSenderId: "508083221053",
+  appId: "1:508083221053:web:8d3f71475c4ead6d11f054",
+};
+
+const db = firebase.initializeApp(config);
+export default db;
